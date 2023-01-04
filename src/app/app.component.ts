cedbc064ce52c518f7e6f136d1692faa92430c3d
annroshanthan <r7tamil@gmail.com>
2023-01-04 15:42:22 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
+import { getMaxListeners } from 'process';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Practical';
+export class AppComponent{
+ 
+  public details:Array<any> = [
+    {name:'ravi',email:'ravi@gmail.com',amount:200},
+    {name:'kamal',email:'kamal@gmail.com',amount:200},
+    {name:'vimal',email:'vimal@gmail.com',amount:200}
+  ];
+
+  form = new FormGroup({
+    name: new FormControl('',[Validators.required]),
+    email: new FormControl('',[Validators.required,Validators.email]),
+    phone: new FormControl('',[Validators.required,Validators.maxLength(10)]),
+    amount: new FormControl('',[Validators.required]),
+  })
+
+  get f():{[key: string]: AbstractControl}{
+    return this.form.controls;
+  }
+  onSubmitform(){
+if (this.form.invalid) return;
+console.log(this.form.value)
+  }
 }
